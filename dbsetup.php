c44c8378f3dd6135bb18df9838786d575ed19781
Daniel Ackerson <ack1@umbc.edu>
2019-07-10 11:13:06 -0400

    fix sql errors

--- dbsetup.php
+++ dbsetup.php
@@ -1,65 +1,57 @@
-
 <?php
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('TonyStark', '9999999', 'Iron Man')";
+$connString = "mysql:host=localhost";
+$user = "root";
+
+$pdo = new PDO($connString,$user);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Setup the database and table, but only if they don't exist.
+$sql = "CREATE DATABASE IF NOT EXISTS OregonTrail";
 $result = $pdo->query($sql);
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('ChrisJimenez', '750', 'Trail guide')";
+// Use the database
+$sql = "USE OregonTrail";
 $result = $pdo->query($sql);
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('DanAckerson', '700', 'Trail guide')";
+$sql = "CREATE TABLE IF NOT EXISTS HighScores (
+    name VARCHAR(20) PRIMARY KEY,
+    score INT NOT NULL,
+    rating VARCHAR(20) NOT NULL);";
 $result = $pdo->query($sql);
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('AlexWhitehead', '690', 'Trail guide')";
-$result = $pdo->query($sql);
-       
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('DustinCuocci', '400', 'Greenhorn')"; 
-$result = $pdo->query($sql);
+// Populate the HighScores table with a few names for display.
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('HarryPotter', '390', 'Wizard')";
-$result = $pdo->query($sql);
+$sql = "
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('TonyStark', '9999999', 'Iron Man');
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('JackSparrow', '375', 'Swabby')";
-$result = $pdo->query($sql);
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('ChrisJimenez', '750', 'Trail guide');
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('ScarletJohannson', '370', 'Widow')";
-$result = $pdo->query($sql);
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('DanielAckerson', '700', 'Trail guide');
 
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('HarveyDent', '690', 'TwoFace')";
-$result = $pdo->query($sql);
-       
-$sql = "INSERT INTO OregonTrail.HighScores 
-        (`name`, `score`, `rating`)
-        values
-        ('ChrisDavis', '100', 'Worthless')"; 
-$result = $pdo->query($sql);
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('AlexWhitehead', '690', 'Trail guide');
 
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('DustinCuocci', '400', 'Greenhorn');
+
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('HarryPotter', '390', 'Wizard');
+
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('JackSparrow', '375', 'Swabby');
+
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('ScarletJohannson', '370', 'Widow');
+
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('HarveyDent', '690', 'TwoFace');
+
+INSERT IGNORE INTO HighScores (`name`, `score`, `rating`)
+VALUES ('ChrisDavis', '100', 'Worthless');";
+$result = $pdo->query($sql);
 
 ?>
--- highscores.php
+++ highscores.php
@@ -8,93 +8,60 @@
 
     <?php
 
-      try{   
+    try {   
+        require_once "dbsetup.php";
 
-        $connString = "mysql:host=localhost";
-        $user = "root";
-
-        $pdo = new PDO($connString,$user);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Setup the database and table, but only if they don't exist.
-        $sql = "CREATE DATABASE IF NOT EXISTS OregonTrail";
-        $result = $pdo->query($sql);
-          
-        $sql = "CREATE TABLE IF NOT EXISTS OregonTrail.HighScores (
-          name varchar(20),
-          score int,
-          rating varchar(20))";
-        $result = $pdo->query($sql);
-
-        // This is optional. The script will populate the HighScores
-        // table with a few names for display.
-        // require "dbsetup.php";
- 
         // If adding new players for testing.
-        if(isset($_POST["submit"])){
+        if (isset($_POST["submit"])) {
 
-          $drop = $_POST['drop'];
-          if($drop == "YES" && strlen($_POST['pname']) == 0 && strlen($_POST['score']) == 0 && strlen($_POST['rating']) == 0){
-            
-            $sql = "DROP TABLE OregonTrail.HighScores";
-            $result = $pdo->query($sql);
+            $drop = $_POST['drop'];
+            if ($drop == "YES" && strlen($_POST['pname']) == 0
+                && strlen($_POST['score']) == 0 && strlen($_POST['rating']) == 0) {
 
-            $sql = "CREATE TABLE IF NOT EXISTS OregonTrail.HighScores (
-              name varchar(20),
-              score int,
-              rating varchar(20))";
-              $result = $pdo->query($sql);
+                $sql = "DROP TABLE HighScores";
+                $result = $pdo->query($sql);
 
-          }
-          
-          else if($drop != "YES" && strlen($_POST['pname']) > 0 && strlen($_POST['score']) > 0 && strlen($_POST['rating']) > 0){
+                $sql = "CREATE TABLE IF NOT EXISTS HighScores (
+                    name varchar(20),
+                    score int,
+                    rating varchar(20))";
+                $result = $pdo->query($sql);
 
-            $sql = "select count(*) from OregonTrail.HighScores";
-            $result = $pdo->query($sql);
-            
-            // Top 10 can only have 10 players!
-            if ($result->fetchColumn() < 10){
+            } else if ($drop != "YES" && strlen($_POST['pname']) > 0
+                && strlen($_POST['score']) > 0 && strlen($_POST['rating']) > 0) {
 
-              $pname = $_POST['pname'];
-              $score = $_POST['score'];
-              $rating = $_POST['rating'];
+                $sql = "select count(*) from HighScores";
+                $result = $pdo->query($sql);
 
-              $sql = "INSERT INTO OregonTrail.HighScores 
-                     (`name`, `score`, `rating`)
-                     values
-                     ('$pname', '$score', '$rating')";
-              $result = $pdo->query($sql);
+                // Top 10 can only have 10 players!
+                if ($result->fetchColumn() < 10) {
 
+                    $pname = $_POST['pname'];
+                    $score = $_POST['score'];
+                    $rating = $_POST['rating'];
+
+                    $sql = "INSERT INTO HighScores 
+                        (`name`, `score`, `rating`)
+                        values
+                        ('$pname', '$score', '$rating')";
+                    $result = $pdo->query($sql);
+
+                } else {
+                    echo "Only 10 players allowed on the high core list. Drop the table.";
+                }
+            } else {
+                echo "Please fill out the form in its entirety. Enter either new player data, or YES to drop the table.";
             }
-
-            else{
-
-              echo "Only 10 players allowed on the high core list. Drop the table.";
-
-            }
-
-          }
-
-          else{
-
-            echo "Please fill out the form in its entirety. Enter either new player data, or YES to drop the table.";
-
-          }
-
         }
- 
-        $sql = "select name, score, rating from OregonTrail.Highscores order by score desc";
+
+        $sql = "select name, score, rating from HighScores order by score desc";
         $result = $pdo->query($sql);
         $rows = $result->fetchAll(PDO::FETCH_ASSOC); 
         $pdo = null;
-
-      }      
-
-      catch (PDOException $e){
-
+    } catch (PDOException $e) {
+        $pdo = null;
         die($e->getMessage());
-
-      }  
+    }  
 
     ?>
 
@@ -102,48 +69,46 @@
         <h1>Oregon Trail Project</h1>
         <div class="container">
             <div class="highscore">
-            The Oregon Top Ten
-            <div class="highscore_table">
-            <table border="0">
-            <?php
+                The Oregon Top Ten
+                <div class="highscore_table">
+                    <table border="0">
+                        <br><br>
+                        <tr><td width='225px;'>
+                        Name
+                        </td><td width='200px;'>
+                        Score
+                        </td><td width='200px;'>
+                        Rating
+                        </td></tr>
+                        <tr height='40px;'></tr>
 
-              echo "<br><br>";
-              echo "<tr><td width='225px;'>";
-              echo "Name";
-              echo "</td><td width='200px;'>";
-              echo "Score";
-              echo "</td><td width='200px;'>";
-              echo "Rating";
-              echo "</td></tr>";
-              echo "<tr height='40px;'></tr>";              
-               
-              foreach ( $rows as $row ) {
+                        <?php
 
-                echo "<tr><td>";
-                echo($row['name']);
-                echo"</td><td>";
-                echo($row['score']);
-                echo "</td><td>";
-                echo($row['rating']);
-                echo("</td></tr>\n");
-              
-              }
+                        foreach ( $rows as $row ) {
 
-            ?>
-            </div>
-            </table>
+                            echo "<tr><td>";
+                            echo($row['name']);
+                            echo"</td><td>";
+                            echo($row['score']);
+                            echo "</td><td>";
+                            echo($row['rating']);
+                            echo("</td></tr>\n");
+
+                        }
+
+                        ?>
+                    </table>
+                </div>
             </div>
         </div>
-    <h2>ADD NAMES TO HIGH SCORE LIST - FOR TESTING ONLY!</h2>
-    <form method="post" action="highscores.php">
-    <br>
-    Name:<input type="text" name="pname"><br>
-    Score:<input type="text" name="score"><br>
-    Rating:<input type="text" name="rating"><br>
-    Drop Table Instead? (Enter YES if so):<input type="text" name="drop"><br>
-    <input type="submit" value="Submit" name="submit">
-    </form>
-    
-    
+        <h2>ADD NAMES TO HIGH SCORE LIST - FOR TESTING ONLY!</h2>
+        <form method="post" action="highscores.php">
+            <br>
+            Name:<input type="text" name="pname"><br>
+            Score:<input type="text" name="score"><br>
+            Rating:<input type="text" name="rating"><br>
+            Drop Table Instead? (Enter YES if so):<input type="text" name="drop"><br>
+            <input type="submit" value="Submit" name="submit">
+        </form>
     </body>
 </html>
