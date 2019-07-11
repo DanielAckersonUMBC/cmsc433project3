5820c9324afdb34a8593542a2429193a35d34f00
cuocci1 <dcuocci1@umbc.edu>
2019-07-11 15:22:50 -0400

    Title Page and Top Ten back/forth

---
+++ drawing.js
@@ -0,0 +1,138 @@
+
+var on_highscore_page = 0;
+var TOP_TEN = 10;
+
+function drawTitle(){
+	
+	// Clear out the canvas.
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0,0,c.width,c.height);
+	
+	// Draws the title screen.	
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	var img = document.getElementById("trail_title");
+	ctx.drawImage(img, 2.5, 10);
+	img = document.getElementById("title_graphic.png");
+	ctx.drawImage(img, 12, 70);
+	ctx.font = "16px AppleII";
+	ctx.fillStyle = "white";
+	ctx.fillText("You may:",80, 170);
+	ctx.fillText("1.  Travel the trail", 107, 210);
+	ctx.fillText("2.  Learn about the trail", 107, 229);
+	ctx.fillText("3.  See the Oregon Top Ten", 107, 248);
+	ctx.fillText("4.  Turn sound off", 107, 267);
+	ctx.fillText("5.  Choose Management Options", 107, 286); 
+	ctx.fillText("6.  End", 107, 305);
+	ctx.fillText("What is your choice?",80, 345);
+	ctx.drawImage(img, 12, 375);
+	  	  
+	// Bring the input box back.
+	document.getElementById("title").setAttribute("type","text");
+	document.getElementById("title").setAttribute("value","");			
+	document.getElementById("title").focus();
+	
+	on_highscore_page = 0;
+	
+}
+
+function addListeners(){
+
+	var title = document.getElementById("title");
+	title = document.getElementById("title");
+	if(title != null){
+	
+	  //alert('listening');
+	  title.addEventListener("keydown", function (e) {
+		  if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+			  handle_title(title.value);
+		  }
+	  });
+	  
+	  	  title.addEventListener("keydown", function (e) {
+		  if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+			  handle_title(title.value);
+		  }
+	  });
+	  
+	  title.addEventListener("focusout", titleFocus);
+	  
+	}	
+
+	
+	document.body.onkeyup = function (e){
+		
+		if (e.keyCode === 32 && on_highscore_page){ // checks user presses space & is on title page.
+		
+			drawTitle();
+			
+		}
+	}
+
+}
+
+function handle_title(val){ 
+
+  // Navigate to high score page.
+  if (val == 3){
+  
+    on_highscore_page = 1;
+	var json;
+	
+    // Get rid of the input box.
+	document.getElementById("title").setAttribute("type","hidden");
+	document.getElementById("title").setAttribute("autofocus","");
+	
+	// Clear out the canvas.
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0,0,c.width,c.height);
+	
+	// Sent a request to the high scores php file.
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+
+		json = JSON.parse(this.responseText);
+		console.log(json);
+		
+		// Draws the highscore screen.
+		var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");	
+		ctx.font = "18px AppleII";
+		ctx.fillStyle = "white";
+		ctx.fillText("The Oregon Top Ten", 165, 50);
+		ctx.fillText("Name", 75, 130);
+		ctx.fillText("Points", 250, 130);
+		ctx.fillText("Rating", 430, 130);
+		
+		// Draw the results of the high score query.
+		var n_x = 10; var p_x = 265; var r_x = 400;
+		var y = 160;
+		for (i = 0; i < TOP_TEN; i++){
+
+		  ctx.fillText(json[i].name, n_x, y);
+		  ctx.fillText(json[i].score, p_x, y);
+		  ctx.fillText(json[i].rating, r_x, y);
+		  
+		  y += 20;
+		
+		}
+		
+		ctx.fillText("Press SPACE BAR to return", 120, 410);
+		
+	  }
+	};
+	xmlhttp.open("GET", "highscores_working.php", true);
+	xmlhttp.send();
+		
+  }
+	
+}
+
+function titleFocus(){
+	
+	document.getElementById("title").focus();
+	
+}
--- highscores.php
+++ highscores.php
@@ -1,87 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'/>
-        <title>Oregon Trail</title>
-        <link rel="stylesheet" href="proj3.css"/>
-    </head>
 
-    <?php
-
-	require_once('dbsetup.php');
-    try {           
-
-        // If adding new players for testing.
-        if (isset($_POST["submit"])) {
-
-            $drop = $_POST['drop'];
-            if ($drop == "YES" && strlen($_POST['pname']) == 0
-                && strlen($_POST['score']) == 0 && strlen($_POST['rating']) == 0) {
-
-                $sql = "DROP TABLE HighScores";
-                $result = $pdo->query($sql);
-
-                $sql = "CREATE TABLE IF NOT EXISTS HighScores (
-                    name varchar(20),
-                    score int,
-                    rating varchar(20))";
-                $result = $pdo->query($sql);
-
-            } else if ($drop != "YES" && strlen($_POST['pname']) > 0
-                && strlen($_POST['score']) > 0 && strlen($_POST['rating']) > 0) {
-
-				$sql = "select name, score, rating from HighScores order by score asc";
-				$result = $pdo->query($sql);
-                $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-				
-				$low_score = $rows[0]['score'];				
-				$new_score = $_POST['score'];	
-				//echo($low_score);    
-				
-				// If the current player is in the top 10.
-				if ($new_score > $low_score){
-					
-					$low_score_name = $rows[0]['name'];
-					$low_score_rating = $rows[0]['rating'];
-					$sql = "delete from HighScores where name = '$low_score_name' and score = '$low_score' and rating = '$low_score_rating'";
-					$result = $pdo->query($sql);
-					
-					$pname = $_POST['pname'];
-					$rating = $_POST['rating'];
-					
-					$sql = "INSERT INTO HighScores 
-                        (`name`, `score`, `rating`)
-                        values
-                        ('$pname', '$new_score', '$rating')";
-                    $result = $pdo->query($sql);
-					
-				}                
-
-            } else {
-                echo "Please fill out the form in its entirety. Enter either new player data, or YES to drop the table.";
-            }
-        }
-        
-    } catch (PDOException $e) {
-        $pdo = null;
-        die($e->getMessage());
-    }  
-	 
-	 // No matter what, when the script runs, the results
-	 // need to be displayed.
-	 $sql = "select name, score, rating from HighScores order by score desc";
-     $result = $pdo->query($sql);
-     $rows = $result->fetchAll(PDO::FETCH_ASSOC); 
-	 $json = json_encode($rows);
-	 echo($json);
-     $pdo = null;
-	
-    ?>
-
-    <body>
+ 
+        <script>		
+		</script>
+	    <?php require_once("highscores_working.php"); ?>
         <h1>Oregon Trail Project</h1>
         <div class="container">
-            <div class="highscore">
+            <div class="highscore"><br><br>
                 The Oregon Top Ten
                 <div class="highscore_table">
                     <table border="0">
@@ -110,10 +34,12 @@
                         }
 
                         ?>
-                    </table>
-                </div>
+                    </table><br><br>
+					<div class="title_text">Press Space to Return to Title Screen</div>
+                </div>	
             </div>
         </div>
+		<!--
         <h2>ADD NAMES TO HIGH SCORE LIST - FOR TESTING ONLY!</h2>
         <form method="post" action="highscores.php">
             <br>
@@ -123,5 +49,6 @@
             Drop Table Instead? (Enter YES if so):<input type="text" name="drop"><br>
             <input type="submit" value="Submit" name="submit">
         </form>
-    </body>
-</html>
+		-->
+		
+
