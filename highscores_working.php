62dbc11c5e660fcbde2fd1a91228a8d563b921c2
cuocci1 <dcuocci1@umbc.edu>
2019-07-10 21:31:33 -0400

    Added SQL to delete lowest score and add new high score.

--- highscores_working.php
+++ highscores_working.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'/>
-        <title>Oregon Trail</title>
-        <link rel="stylesheet" href="proj3.css"/>
-    </head>
+<?php
 
-    <?php
 
 	require_once('dbsetup.php');
     try {           
@@ -23,7 +16,7 @@
 
                 $sql = "CREATE TABLE IF NOT EXISTS HighScores (
                     name varchar(20),
-                    score int,
+                    score int, 
                     rating varchar(20))";
                 $result = $pdo->query($sql);
 
@@ -75,53 +68,5 @@
 	 $json = json_encode($rows);
 	 echo($json);
      $pdo = null;
-	
-    ?>
-
-    <body>
-        <h1>Oregon Trail Project</h1>
-        <div class="container">
-            <div class="highscore">
-                The Oregon Top Ten
-                <div class="highscore_table">
-                    <table border="0">
-                        <br><br>
-                        <tr><td width='225px;'>
-                        Name
-                        </td><td width='200px;'>
-                        Score
-                        </td><td width='200px;'>
-                        Rating
-                        </td></tr>
-                        <tr height='40px;'></tr>
-
-                        <?php
-
-                        foreach ( $rows as $row ) {
-
-                            echo "<tr><td>";
-                            echo($row['name']);
-                            echo"</td><td>";
-                            echo($row['score']);
-                            echo "</td><td>";
-                            echo($row['rating']);
-                            echo("</td></tr>\n");
-
-                        }
-
-                        ?>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <h2>ADD NAMES TO HIGH SCORE LIST - FOR TESTING ONLY!</h2>
-        <form method="post" action="highscores.php">
-            <br>
-            Name:<input type="text" name="pname"><br>
-            Score:<input type="text" name="score"><br>
-            Rating:<input type="text" name="rating"><br>
-            Drop Table Instead? (Enter YES if so):<input type="text" name="drop"><br>
-            <input type="submit" value="Submit" name="submit">
-        </form>
-    </body>
-</html>
+	 
+	 ?>
